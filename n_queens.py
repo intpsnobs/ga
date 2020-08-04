b087b3991e4555771e9f643bff6853869d952e3e
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2020-08-04 18:37:59 -0300

    fix : run black

--- n_queens.py
+++ n_queens.py
@@ -6,30 +6,32 @@ import galapagos.operators.mutation as mt
 import galapagos.selector as sl
 from galapagos.log_handler import plot_runs, convergence_chart
 
+
 def show_table(chromosome: np.ndarray) -> str:
     size = len(chromosome)
-    board = np.full((size, size), '.')
+    board = np.full((size, size), ".")
 
     for index, item in enumerate(chromosome):
-        board[index][item-1] = 'x'
+        board[index][item - 1] = "x"
 
     out = ""
     for row in board:
         out += "|"
         for tile in row:
             out += tile
-        out +=" |\n"
+        out += " |\n"
 
     return out
 
+
 def fitness(individual: ndarray):
     dim = len(individual)
-    max_fit = dim*(dim-1)
+    max_fit = dim * (dim - 1)
     fit = max_fit
-    for i in range(len(individual)-1):
-        for j in range(i+1, len(individual)):
-            fit -= 2*(abs(individual[i]-individual[j]) == abs(i-j))
-    return individual, fit/max_fit
+    for i in range(len(individual) - 1):
+        for j in range(i + 1, len(individual)):
+            fit -= 2 * (abs(individual[i] - individual[j]) == abs(i - j))
+    return individual, fit / max_fit
 
 
 if __name__ == "__main__":
@@ -39,9 +41,8 @@ if __name__ == "__main__":
         crossover_function=cr.pmx,
         select_function=sl.stochastic_tournament,
         config_file="n_queens.cfg",
-        phenotype=show_table
+        phenotype=show_table,
     )
 
-    # print(result)
     convergence_chart(result, False)
     plot_runs(result)
--- radio.py
+++ radio.py
@@ -13,56 +13,55 @@ from galapagos.log_handler import plot_runs, convergence_chart
 # L = Radio Luxo: 2/dia -> R$40 (Restricao: max 32 funcionarios)
 # f(x) =
 
+
 def show(chromosome: ndarray):
     rl, rs = normalization(chromosome)
 
-    out = f'Luxury radio: {rl}\n'
-    out += f'Standard radio: {rs}\n'
-    out += f'Profit: {(40*rl+30*rs)}'
+    out = f"Luxury radio: {rl}\n"
+    out += f"Standard radio: {rs}\n"
+    out += f"Profit: {(40*rl+30*rs)}"
 
     return out
 
 
-
 def normalization(ind: ndarray):
-    xrl = binaryToInt(ind[:len(ind)//2])
-    xrs = binaryToInt(ind[len(ind)//2:])
+    xrl = binaryToInt(ind[: len(ind) // 2])
+    xrs = binaryToInt(ind[len(ind) // 2 :])
     # rs [0-24] -> 5 bits
     # rl [0-16] -> 5 bits
-    rl = math.floor(0+(16/31)*xrl)
-    rs = math.floor(0+(24/31)*xrs)
+    rl = math.floor(0 + (16 / 31) * xrl)
+    rs = math.floor(0 + (24 / 31) * xrs)
 
     return rl, rs
+
 
 def fitness(ind: ndarray):
     rl, rs = normalization(ind)
 
     r = -1
-    FOn = (30*rs + 40*rl)/1360
-    Hn = max(0, (rs + 2*rl - 40))/16
-    fit = FOn + r*Hn
+    FOn = (30 * rs + 40 * rl) / 1360
+    Hn = max(0, (rs + 2 * rl - 40)) / 16
+    fit = FOn + r * Hn
 
     return ind, fit
 
 
 if __name__ == "__main__":
 
-    
     result = gp.run(
         fitness,
         mutation_function=mt.bit_flip,
         crossover_function=cr.two_points,
         select_function=sl.fitness_proportionate_selection,
         config_file="radio.cfg",
-        phenotype=show
+        phenotype=show,
     )
-    
 
     convergence_chart(result, False)
     plot_runs(result)
 
 
-'''
+"""
 conjunto: radio standart, luxo
 dominio:
 rs [0-24] -> 5 bits
@@ -80,4 +79,4 @@ r = -1
 FOn = (30RS + 40RL)/1360
 
 Hn = max(0, RS + 2RL - 40)/16
-'''
+"""
